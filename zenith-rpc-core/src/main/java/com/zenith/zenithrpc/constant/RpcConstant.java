7cca1d034d1fedb69b0ab1b54e9dd165f82d61ac
zenith <zenith>
2024-08-15 21:23:18 +0800

    :sparkles: 全局配置加载

---
+++ zenith-rpc-core/src/main/java/com/zenith/zenithrpc/constant/RpcConstant.java
@@ -0,0 +1,15 @@
+package com.zenith.zenithrpc.constant;
+
+/**
+ * RPC 相关常量
+ *
+ * @author zenith
+ * @date 2024/07/27
+ */
+public interface RpcConstant {
+
+    /**
+     * 默认配置文件加载前缀
+     */
+    String DEFAULT_CONFIG_PREFIX  = "rpc";
+}
